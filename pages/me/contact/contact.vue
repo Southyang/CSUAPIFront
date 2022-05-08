976e00d9b0b4655ae604ce6f2daf1e6f6427eee1
Southyang <south_yang99@163.com>
2022-05-08 10:58:41 +0800

    modify contact 20220508

--- pages/me/contact/contact.vue
+++ pages/me/contact/contact.vue
@@ -6,7 +6,9 @@
 			暂无
 		</view>
 		<uni-section title="我要反馈" sub-title="Give feedback" type="line"></uni-section>
-		<button type="primary" @click="giveFeedback">去反馈</button>
+		<view class="feedback">
+			<a href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzkzNTI3OTkxOA==&scene=110==#wechat_redirect">去反馈</a>
+		</view>
 	</view>
 </template>
 
@@ -19,11 +21,11 @@
 		},
 		methods: {
 			giveFeedback() {
-				uni.showToast({
-					title: '在开发了！',
-					image: '../../../static/image/notice/weep.png',
-					duration: 3000
-				});
+				// uni.showToast({
+				// 	title: '在开发了！',
+				// 	image: '../../../static/image/notice/weep.png',
+				// 	duration: 3000
+				// });
 			}
 		}
 	}
@@ -39,5 +41,24 @@
 
 	.contact_question {
 		margin: 10px;
+	}
+	
+	.feedback{
+		background-color: #26a1ff;
+		width: 100vw;
+		height: 40px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	
+	.feedback a{
+		width: inherit;
+		height: inherit;
+		color: #FFFFFF;
+		text-decoration: none;
+		font-size: 25px;
+		text-align: center;
+		line-height: 40px;
 	}
 </style>
